3c75dde7ec7f2c3374dc2819fac07bd3db8d49a4
Jingbo@li <18811145767@163.com>
2018-08-04 10:30:21 +0800

    Create New Project
    Create New Xcode Project

---
+++ MVVM+RAC/MVVM+RAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM+RAC
+//
+//  Created by YWKJ on 2018/8/4.
+//  Copyright © 2018年 YWKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
